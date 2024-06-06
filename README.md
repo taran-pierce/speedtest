88aa1ae49e78ea9078ce3f2767a897d204138b6c
Taran Pierce <taran.tpdesign@gmail.com>
2024-06-06 09:12:16 -0600

    main
    refactoring

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ CLI interface to test your internet connection speed. Using Python 3 and speedte
 - speedtest-cli
 - pymongo
 
+### Important
+- Will need to generate CA cert and key if they are not there to include for MongoDB
+- Need to create "config.ini" and it needs USERNAME and PASSWORD
--- main.py
+++ main.py
@@ -1,19 +1,59 @@
+# config data
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+DB_USERNAME = config["database"]["USERNAME"]
+DB_PASSWORD = config["database"]["PASSWORD"]
+
+# import dependencies
 import speedtest
 import os
 import pymongo
 import datetime
+import certifi
+from pymongo.server_api import ServerApi
 
-client = pymongo.MongoClient('mongodb://localhost:27017/')
+# get CA cert using "certifi"
+ca_cert_path = certifi.where()
 
+# set up mongodb URI
+uri = f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@cluster0.94s77tq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+# setting TLS and adding in CAFile path for the cert
+client = pymongo.MongoClient(uri,
+  tls=True,
+  tlsCAFile=ca_cert_path,
+  server_api=ServerApi(
+    version='1',
+  ))
+
+# grab correct db and collection
 db = client["speedtest"]
-
 collection = db["results"]
 
-st = speedtest.Speedtest()
-server_names = []
-st.get_servers(server_names)
+program_on = True
+
+# initialize speedtest-cli
+# it is free, so we get rate limited
+try:
+  st = speedtest.Speedtest()
+  server_names = []
+  st.get_servers(server_names)
+except Exception as e:
+  print("Error with Speedtest-CLI\n")
+  print(f"Erorr: {e}\n")
+  print("Please try again later...\n")
+
+  # not much we can do if we cant use speedtest-cli
+  program_on = False
 
 def clear_screen():
+  """
+  Function to clear terminal screen
+  """
   if os.name == 'nt':
     os.system('cls')
   else:
@@ -65,95 +105,107 @@ def calculate_megabytes(bytes):
   return megabytes
 
 def update_db(type, data):
-  document = {
-    "type": type,
-    "speed": data,
-    "date": datetime.datetime.now().date().strftime("%m-%d-%Y"),
-    "time": datetime.datetime.now().time().strftime("%I:%H:%M"),
-  }
-
-  print("Inserting the following data...\n")
-
-  for data in document:
-    print(f"{data} - {document[data]}")
-
-  collection.insert_one(document)
-
-user_option = int(input(r'''What type of internet speed do you want to test?
-                        
-1 - Download
-                        
-2 - Upload
-
-3 - Ping
-                        
-4 - All three
-                        
-5 - Print previous results
-                        
-Enter the number for your choice: 
-'''))
-
-clear_screen()
-
-results = {}
-
-if user_option == 1:
-  print("\n")
-
-  download = get_download_speed()
-
-  update_db("Download", download)
-
-  print(f"Download: {download}")
-elif user_option == 2:
-  print("\n")
-
-  upload = get_upload_speed()
-
-  update_db("Upload", upload)
-
-  print(f"Upload: {upload}")
-elif user_option == 3:
-  print("\n")
-  ping = get_ping()
-
-  update_db("Ping", ping)
-
-  print(f"Ping: {ping}")
-elif user_option == 4:
-  print("\n")
-
-  results = {
-    "Download": get_download_speed(),
-    "Upload": get_upload_speed(),
-    "Ping": get_ping(),
-  }
-
-  for result in results:
-    print(f"{result}: {results[result]}\n")
-    update_db(result, results[result])
-
-elif user_option == 5:
-  print(f"Previous Results\n")
-  
   try:
-    documents = collection.find()
+    # create dictionary to insert into db
+    document = {
+      "type": type,
+      "speed": data,
+      "date": datetime.datetime.now().date().strftime("%m-%d-%Y"),
+      "time": datetime.datetime.now().time().strftime("%I:%H:%M"),
+    }
 
-    for document in documents:
-      print(f"""
-        Date: {document["date"]}
-        Time: {document["time"]}
-        Type: {document["type"]}
-        Speed: {document["speed"]}
-      """)
+    # For CL, printing out values being added
+    print("Inserting the following data...\n")
+
+    for data in document:
+      print(f"{data} - {document[data]}")
+
+    # insert into db collection
+    collection.insert_one(document)
+  except Exception as e:
+    print(e)
+
+while program_on == True:
+  user_option = int(input(r'''What type of internet speed do you want to test?
+                          
+  1 - Download
+                          
+  2 - Upload
+
+  3 - Ping
+                          
+  4 - All three
+                          
+  5 - Print previous results
+                          
+  Enter the number for your choice: 
+  '''))
+
+  clear_screen()
+
+  results = {}
+
+  if user_option == 1:
+    print("\n")
+
+    download = get_download_speed()
+
+    update_db("Download", download)
+
+    print(f"Download: {download}")
+  elif user_option == 2:
+    print("\n")
+
+    upload = get_upload_speed()
+
+    update_db("Upload", upload)
+
+    print(f"Upload: {upload}")
+  elif user_option == 3:
+    print("\n")
+    ping = get_ping()
+
+    update_db("Ping", ping)
+
+    print(f"Ping: {ping}")
+  elif user_option == 4:
+    print("\n")
+
+    results = {
+      "Download": get_download_speed(),
+      "Upload": get_upload_speed(),
+      "Ping": get_ping(),
+    }
+
+    for result in results:
+      print(f"{result}: {results[result]}\n")
+      update_db(result, results[result])
+  elif user_option == 5:
+    print(f"Previous Results\n")
+    
+    try:
+      documents = collection.find()
+
+      for document in documents:
+        print(f"""
+          Date: {document["date"]}
+          Time: {document["time"]}
+          Type: {document["type"]}
+          Speed: {document["speed"]}
+        """)
 
 
-  except pymongo.errors.PyMongoError as e:
-    print(f"An error occured: {e}")
-else:
-  print(f"Invalid Option: {user_option}.")
-  print("Please enter: 1, 2, 3, 4, or 5")
+    except pymongo.errors.PyMongoError as e:
+      print(f"An error occured: {e}")
+  else:
+    print(f"Invalid Option: {user_option}.")
+    print("Please enter: 1, 2, 3, 4, or 5")
+  proceed = input("""Do you want to run another test?
+    Enter "Yes" or "No" 
+  """)
+
+  if proceed.lower() == "no":
+    program_on = False
 
 client.close()
 
